6140dd8a299b20cbac1edfbdfc9c4d0215a8bc77
witsaint <gao973738@qq.com>
2025-10-03 22:43:48 +0800

    chore: update pnpm-lock and workspace files, add gradient-string dependency, and refactor banner rendering

--- packages/pilotage/src/ui/banner.tsx
+++ packages/pilotage/src/ui/banner.tsx
@@ -1,10 +1,8 @@
 import process from 'node:process'
-import { render } from 'ink'
-import React from 'react'
-import { LogoDesc } from './logo-desc'
+import gradient from 'gradient-string'
 
-export function renderBanner(): void {
-  const shell = `                       
+function getLogoStr(): string {
+  return `                       
  _|_|_|    _|  _|              _|                                    
  _|    _|      _|    _|_|    _|_|_|_|    _|_|_|    _|_|_|    _|_|    
  _|_|_|    _|  _|  _|    _|    _|      _|    _|  _|    _|  _|_|_|_|  
@@ -12,12 +10,48 @@ export function renderBanner(): void {
  _|        _|  _|    _|_|        _|_|    _|_|_|    _|_|_|    _|_|_|  
                                                        _|            
                                                    _|_|                                                                                                                   
-  `
+`
+}
+
+function padText(originStr: string, totalLen: number, splitStr: string) {
+  const startStr = splitStr.padStart(totalLen, ' ')
+  
+  return startStr + originStr
+}
+
+function getLogo(version: string): string {
+  const logo = getLogoStr();
+
+  
+  const _logoStrs = logo.split('\n')
+  const maxLen = 70;
+
+  const welLine = _logoStrs.length - 3
+  const versionLine = _logoStrs.length - 2
+
+  const versionStr = `Version: ${version}`
+  
+  const logoStrs = _logoStrs.map((item, idx) => {
+    let curLenStr = item.padEnd(maxLen, ' ')
+    if (idx === welLine) {
+      curLenStr = curLenStr.slice(19)
+    }
+    if (idx === versionLine) {
+      curLenStr = curLenStr.slice(versionStr.length)
+    }
+    return gradient(['cyan', '#255B53']).multiline(curLenStr)
+  })
+
+  
+  logoStrs[welLine] = padText(logoStrs[welLine], 19, gradient(['#0087FF', 'pink'])('Welcome to Politage'))
+  logoStrs[versionLine] = padText(logoStrs[versionLine], 5, gradient(['#0087FF', 'pink'])(versionStr))
+  return logoStrs.join('\n')
+}
+
+export function renderBanner(): void {
+  const shell = getLogo('0.0.2')
 
   // Print ASCII art first
   process.stdout.write(shell)
   process.stdout.write('\n')
-
-  // Then render Ink component
-  render(<LogoDesc />)
 }
